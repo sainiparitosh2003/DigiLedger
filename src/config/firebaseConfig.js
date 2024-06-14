f561193684d9650a165463e71197f4207a019d82
sainiparitosh2003 <sainiparitosh2003@gmail.com>
2024-06-14 23:25:30 +0530

    first commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC4Z_5Q3Ukp8m4JfWblqpnB2_thAKmqzsc",
-    authDomain: "digiledger-7df30.firebaseapp.com",
-    projectId: "digiledger-7df30",
-    storageBucket: "digiledger-7df30.appspot.com",
-    messagingSenderId: "572859952631",
-    appId: "1:572859952631:web:598483dfe8ef17a87752f2"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import  CryptoContext  from './CryptoContext';
+import CryptoContext from './CryptoContext';
 import "react-alice-carousel/lib/alice-carousel.css";
+
+// Error Boundary Component
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+// Performance measurement function
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <CryptoContext>
-    <App />
-    </CryptoContext>
+    <ErrorBoundary>
+      <CryptoContext>
+        <App />
+      </CryptoContext>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app
+reportWebVitals(console.log);
